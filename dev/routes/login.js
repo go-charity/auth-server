e356bb6facea6cee7d9207e3883097dd8869d427
onukwilip <onukwilip@gmail.com>
2023-09-25 02:24:42 +0100

    Removed the dev folder from the remote repo

--- dev/routes/login.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const login_1 = require("../controllers/login");
-const utils_1 = require("../utils/utils");
-const loginRoutes = (0, express_1.Router)();
-loginRoutes.post("/", utils_1.validateApiKey, login_1.loginUser);
-exports.default = loginRoutes;
--- dev/routes/refresh_token.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const refresh_token_1 = require("../controllers/refresh_token");
-const utils_1 = require("../utils/utils");
-const refreshTokenRoutes = (0, express_1.Router)();
-refreshTokenRoutes.post("/", utils_1.validateApiKey, refresh_token_1.refreshToken);
-exports.default = refreshTokenRoutes;
