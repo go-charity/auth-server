4033e29733c13d218c9ee31a6143df8de6c2cc5c
onukwilip <onukwilip@gmail.com>
2024-06-09 07:37:15 +0100

    Modified the app's port

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import app from "./app";
 
-app.listen(process.env.PORT || 5000, () =>
-  console.log(`Auth server listening on port ${process.env.PORT || 5000}`)
+app.listen(process.env.PORT, () =>
+  console.log(`Auth server listening on port ${process.env.PORT}`)
 );
